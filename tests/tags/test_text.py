e67fd1a9b1031a68ca18197a096bdb18ca62db38
soasme <soasme@gmail.com>
2015-08-13 09:08:28 +0800

    Refactor render_layout, patch_layout test.

--- tests/tags/test_text.py
+++ tests/tags/test_text.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from riot.layout import render_layout
+from copy import deepcopy
+
+from riot.layout import render_layout, patch_layout
 from pytest import fixture
 
 @fixture
@@ -13,62 +15,69 @@ def test_text_tag(layout):
         'text',
         {
             'markup': 'hello world',
+            'align': 'left',
+            'wrap': 'space',
         }
     ]
 
 def test_text_tag_with_opts_ref(layout):
-    opts = {'title': 'Hello World'}
+    opts = {'title': 'hello world'}
     layout[2] = '{ opts.title }'
     el = render_layout(layout, opts)
     assert el == [
         'text',
         {
             'markup': 'hello world',
+            'align': 'left',
+            'wrap': 'space',
         }
     ]
 
-def test_text_tag_with_self_ref():
+def test_text_tag_with_attr_ref(layout):
+    layout[1]['title'] = 'hello world'
+    layout[2] = '{ opts.title }'
+    el = render_layout(layout)
+    assert el == [
+        'text',
+        {
+            'markup': 'hello world',
+            'align': 'left',
+            'wrap': 'space',
+        }
+    ]
+
+def test_text_tag_with_self_ref(layout):
     layout[2] = '{ title }'
     el = render_layout(layout, {}, {'title': 'hello world'})
     assert el == [
         'text',
         {
             'markup': 'hello world',
-        }
-    ]
-
-def test_text_tag_with_attr(layout):
-    layout[1]['title'] = 'hello world'
-    layout[2] = '{ opts.title }'
-    assert el == [
-        'text',
-        {
-            'markup': 'hello world',
+            'align': 'left',
+            'wrap': 'space',
         }
     ]
 
 def test_update_layout(layout):
-    modified = deepcopy(layout)
-    modified[1]['align'] = 'left'
-    modified[1]['wrap'] = 'space'
-    modified[2] = 'world updated'
     el = [
         'text',
         {
             'markup': 'hello world',
+            'wrap': 'space'
         }
     ]
-    el, patch = update_layout(el, layout)
-    assert el == [
+    el2 = [
         'text',
         {
             'markup': 'world updated',
+            'align': 'left',
+            'wrap': 'any'
         }
     ]
-    assert patch == [
-        ('set_align_mode', 'left'),
-        ('set_wrap_mode', 'space'),
+    assert patch_layout(el, el2) == [
+        # ('set_align_mode', 'left'), // ignore default
         ('set_text', 'world updated'),
+        ('set_wrap_mode', 'any'),
     ]
 
 def test_update_layout_no_change(layout):
@@ -78,46 +87,5 @@ def test_update_layout_no_change(layout):
             'markup': 'hello world',
         }
     ]
-    el_, patch = update_layout(el, layout)
-    assert el == el_
-    assert patch == []
-
-def test_update_layout_by_opts(layout):
-    modified = deepcopy(layout)
-    modified[2] = '{ opts.title }'
-    el = [
-        'text',
-        {
-            'markup': 'hello world',
-        }
-    ]
-    el, patch = update_layout(el, layout, {'title': 'world updated'})
-    assert el == [
-        'text',
-        {
-            'markup': 'world updated',
-        }
-    ]
-    assert patch == [
-        ('set_text', 'world updated'),
-    ]
-
-def test_update_layout_by_self(layout):
-    modified = deepcopy(layout)
-    modified[2] = '{ title }'
-    el = [
-        'text',
-        {
-            'markup': 'hello world',
-        }
-    ]
-    el, patch = update_layout(el, layout, {}, {'title': 'world updated'})
-    assert el == [
-        'text',
-        {
-            'markup': 'world updated',
-        }
-    ]
-    assert patch == [
-        ('set_text', 'world updated'),
-    ]
+    el2 = deepcopy(el)
+    assert patch_layout(el, el2) == []
