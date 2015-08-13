dbf61f499f3988190ecf5f1d838824e37c31f707
soasme <soasme@gmail.com>
2015-08-13 09:08:46 +0800

    Reimplement render_text_layout, patch_text_layout.

--- riot/layout.py
+++ riot/layout.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+from copy import deepcopy
 from uuid import uuid4
 from urwid import (
     Text, Divider
@@ -10,7 +11,9 @@ def get_attribute(render_data, variable):
     levels = variable.split('.')
     r = render_data
     for level in levels:
-        r = r[level]
+        r = r.get(level) or {}
+    if not r:
+        return ''
     return r
 
 variable_re = re.compile(r'({\s*.*\s*})')
@@ -22,19 +25,33 @@ def render_template(text, render_data):
     return text
 
 def render_text_layout(attributes, text, opts, ref):
-    class_ = attributes.get('class', '')
-    classes = [] if not class_ else [class_]
-    align = attributes.get('align', 'left')
-    wrap = attributes.get('wrap', 'any')
+    align = attributes.pop('align', 'left')
+    wrap = attributes.pop('wrap', 'space')
     render_data = dict(ref)
-    render_data['opts'] = opts
+    render_data['opts'] = deepcopy(opts)
+    render_data['opts'].update(attributes)
     if isinstance(text, str):
         markup = render_template(text, render_data)
     elif isinstance(text, list):
         markup = [render_template(tpl, render_data) for tpl in text if tpl]
     elif isinstance(text, tuple):
         markup = render_template(text[1], render_data)
-    return Text(markup, align=align, wrap=wrap)
+    return [
+        'text',
+        {
+            'markup': markup,
+            'align': align,
+            'wrap': wrap,
+        }
+    ]
+
+def patch_text_layout(el1, el2):
+    if el1[1].get('markup', '') != el2[1].get('markup', ''):
+        yield ('set_text', el2[1]['markup'])
+    if el1[1].get('align', 'left') != el2[1].get('align', 'left'):
+        yield ('set_align_mode', el2[1].get('align', 'left'))
+    if el1[1].get('wrap', 'space') != el2[1].get('wrap', 'space'):
+        yield ('set_wrap_mode', el2[1].get('wrap', 'space'))
 
 def render_div_layout(attributes, text, opts, ref):
     div_char = attributes.get('char', u' ')
@@ -47,9 +64,14 @@ RENDERERS = {
     'div': render_div_layout,
 }
 
+PATCHERS = {
+    'text': patch_text_layout,
+}
+
 def render_layout(layout, opts={}, ref={}):
     tag_name, attributes, children = layout
-    el = RENDERERS[tag_name](attributes, children, opts, ref)
-    el.uuid = uuid4()
-    return el
+    return RENDERERS[tag_name](attributes, children, opts, ref)
 
+def patch_layout(el1, el2):
+    assert(el1[0] == el2[0])
+    return [_ for _ in PATCHERS[el2[0]](el1, el2)]
