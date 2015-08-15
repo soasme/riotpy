c2c87e12761f51c1e3bd54e9e1167dbd15e08665
soasme <soasme@gmail.com>
2015-08-16 00:03:30 +0800

    Rewrite minimal application app.

--- examples/minimal_application/app.py
+++ examples/minimal_application/app.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import urwid
-from riot.tags.tags import parse_tag_from_string
 
-fill = parse_tag_from_string('<filler valign="top"><text>hello world!</text></filler>')
-loop = urwid.MainLoop(fill)
-loop.run()
+from riot.tags.tags import parse_tag_from_string
+from riot.app import run_tag
+
+run_tag(
+    parse_tag_from_string(
+        '''
+        <filler valign="middle">
+          <text>hello world!</text>
+        </filler>
+        '''
+    )
+)
