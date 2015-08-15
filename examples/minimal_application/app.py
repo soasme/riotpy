a004781c405cdf0bce3493110814bca281e37e6f
soasme <soasme@gmail.com>
2015-08-16 00:14:23 +0800

    Remove urwid import.

--- examples/minimal_application/app.py
+++ examples/minimal_application/app.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import urwid
-
 from riot.tags.tags import parse_tag_from_string
 from riot.app import run_tag
 
 run_tag(
     parse_tag_from_string(
         '''
-        <filler valign="middle">
+        <filler valign="top">
           <text>hello world!</text>
         </filler>
         '''
