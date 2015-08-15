42a17793e26c1790bf4ddb8138ba04ebe0b3666c
soasme <soasme@gmail.com>
2015-08-15 23:34:37 +0800

    Wrap text node with pyquery.

--- riot/tags/text.py
+++ riot/tags/text.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from urwid import Text
+from pyquery import PyQuery
 
 def parse_tag_from_string(string):
-    return Text('hello world')
+    return parse_tag_from_node(convert_string_to_node(string))
+
+def parse_tag_from_node(node):
+    return Text(node.text())
+
+def convert_string_to_node(string):
+    return PyQuery(string)
