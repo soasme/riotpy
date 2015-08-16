5b4ba4e6cbb6cae1793c699a540aecb64236ca34
soasme <soasme@gmail.com>
2015-08-16 18:55:00 +0800

    Set default property screen 256 colors.

--- riot/app.py
+++ riot/app.py
@@ -4,6 +4,7 @@ import urwid
 
 def run_tag(tag, *args, **kwargs):
     loop = urwid.MainLoop(tag, *args, **kwargs)
+    loop.screen.set_terminal_properties(colors=256)
     loop.run()
 
 def quit_app():
