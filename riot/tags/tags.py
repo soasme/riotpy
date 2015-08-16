0747c28bae07afdb723821d9f6ff72a1594046f2
soasme <soasme@gmail.com>
2015-08-16 18:54:54 +0800

    Define mount.

--- riot/tags/tags.py
+++ riot/tags/tags.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from . import text, filler
+from pyquery import PyQuery
+from . import text, filler, div, pile, solidfill
 from .utils import convert_string_to_node, detect_class
+
+convert_to_node = convert_string_to_node
 
 def parse_tag_from_string(string):
     return parse_tag_from_node(convert_string_to_node(string))
@@ -9,9 +12,24 @@ def parse_tag_from_string(string):
 @detect_class
 def parse_tag_from_node(node):
     tagname = node[0].tag
+    is_mounted = node.attr.__riot_is_mounted__ == "true"
+    if is_mounted:
+        return parse_tag_from_node(convert_to_node(node.children()))
     if tagname == 'text':
         return text.parse_tag_from_node(node)
     elif tagname == 'filler':
         return filler.parse_tag_from_node(node)
+    elif tagname == 'div':
+        return div.parse_tag_from_node(node)
+    elif tagname == 'pile':
+        return pile.parse_tag_from_node(node)
+    elif tagname == 'solidfill':
+        return solidfill.parse_tag_from_node(node)
     else:
-        raise NotImplementedError
+        raise NotImplementedError(tagname)
+
+def riot_mount(dom, selector, node):
+    pq = dom(selector)
+    pq.attr.__riot_is_mounted__ = 'true'
+    pq.html(node.html())
+    return dom
