c773ac303297ea748f910fe12421c1e5e06b1864
soasme <soasme@gmail.com>
2015-08-17 10:35:26 +0800

    Import edit to tags.

--- riot/tags/tags.py
+++ riot/tags/tags.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 from pyquery import PyQuery
-from . import text, filler, div, pile, solidfill
+
+from . import text, filler, div, pile, solidfill, edit
 from .utils import convert_string_to_node, detect_class
 
 convert_to_node = convert_string_to_node
@@ -25,10 +28,12 @@ def parse_tag_from_node(node):
         return pile.parse_tag_from_node(node)
     elif tagname == 'solidfill':
         return solidfill.parse_tag_from_node(node)
+    elif tagname == 'edit':
+        return edit.parse_tag_from_node(node)
     else:
         raise NotImplementedError(tagname)
 
-def riot_mount(dom, selector, node):
+def riot_mount(dom, selector, node, mount_args={}):
     pq = dom(selector)
     pq.attr.__riot_is_mounted__ = 'true'
     pq.html(node.html())
