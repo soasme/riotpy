7cf5c0c40577cad4be7be0a360d6c89b030a762e
soasme <soasme@gmail.com>
2015-08-18 18:41:41 +0800

    Define mount_to.

--- riot/tags/tags.py
+++ riot/tags/tags.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import re
-
+from uuid import uuid4
 from pyquery import PyQuery
 
 from . import text, filler, div, pile, solidfill, edit
+from ..observable import Observable
 from .utils import convert_string_to_node, detect_class
 
 convert_to_node = convert_string_to_node
@@ -38,3 +39,35 @@ def riot_mount(dom, selector, node, mount_args={}):
     pq.attr.__riot_is_mounted__ = 'true'
     pq.html(node.html())
     return dom
+
+TAG_IMPL = {}
+VDOM = {}
+def mounte_to(root, tagname, opts):
+    impl = TAG_IMPL.get(tagname)
+    if not impl:
+        return
+    inner_html = root.html() or ''
+    root.html('')
+    tag = Observable()
+    tag.uuid = uuid4()
+    tag.impl = impl
+    tag.conf = {
+        'root': root,
+        'opts': opts
+    }
+    @tag.on('unmounted')
+    def on_tag_unmounted(tag):
+        del VDOM[tag.uuid]
+
+def define_tag(name, html, fn):
+    TAG_IMPL[name] = dict(
+        name=name,
+        html=html,
+        fn=fn,
+    )
+    return name
+
+def mount(root, selector, tagname, opts):
+    elements = root(selector)
+    if not elements:
+        return []
