c0eb3ffd51d194109ac8fffee396095b1433010b
soasme <soasme@gmail.com>
2015-08-21 16:52:16 +0800

    Test new node.

---
+++ tests/test_virtual_node.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import riot.virtual_node as vnode
+from riot.observable import Observable
+
+def test_new_node():
+    impl = {'html': '<custom><text>_</text></custom>', 'name': 'custom', 'fn': lambda *a, **kw: None}
+    node = vnode.new_node(impl, '')
+    assert node
+    assert node.uuid
+    for life_cycle_function in ('update', 'mount', 'unmount'):
+        assert getattr(node, life_cycle_function)
+
+def test_mount_children_nodes(node):
+    pass
