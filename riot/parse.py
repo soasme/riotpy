45f55e99bfc54a3408e7dfb892f80ddae307c0ca
soasme <soasme@gmail.com>
2015-08-21 16:49:30 +0800

    Extract logic from script part.

--- riot/parse.py
+++ riot/parse.py
@@ -2,8 +2,26 @@
 
 import types
 import re
+from pyquery import PyQuery
 
 from .observable import Observable
+
+def extract_logic(html):
+    node = PyQuery(html)
+    script = node('script').eq(0)
+    script_text = script.html() or ''
+    scripts = script_text.split('\n')
+    scripts = [sc for sc in scripts if sc]
+    if not scripts:
+        return ''
+    indent = 0
+    for char in scripts[0]:
+        if char == ' ':
+            indent += 1
+        else:
+            break
+    scripts = [sc[indent:] for sc in scripts]
+    return '\n'.join(scripts)
 
 def attach_logic_to_node(script, node, opts):
     self = node
