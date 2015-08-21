964761f1d2464fc2fe093c90a8fec452c7d70e50
soasme <soasme@gmail.com>
2015-08-21 16:51:42 +0800

    Fix test.

--- tests/test_virtal_dom.py
+++ tests/test_virtal_dom.py
@@ -7,12 +7,10 @@ from riot import virtual_dom as vdom
 from riot.observable import Observable
 
 def test_defind_and_get_tag():
-    callback = lambda *a, **kw: None
-    vdom.define_tag('test', '<test></test>', callback)
+    vdom.define_tag('test', '<test></test>')
     assert vdom.get_tag('test') == {
         'name': 'test',
         'html': '<test></test>',
-        'fn': callback
     }
 
 def test_pop_html():
@@ -21,7 +19,7 @@ def test_pop_html():
     assert not node.html()
 
 def test_cache_dom():
-    dom = vdom.new_dom('', '', '', '')
+    dom = Observable()
     dom.uuid = 'dom'
     vdom.cache_dom(dom)
     assert vdom.get_dom(dom.uuid) == dom
@@ -31,22 +29,15 @@ def test_cache_dom():
 
 def test_mount_tag():
     root = PyQuery('<root></root>')
-    tag = {'html': '<custom>hello world</custom>'}
-    dom = vdom.mount_tag(root, tag, {})
+    tag = {'name': 'custom', 'html': '<custom><text>{opts.txt}</text></custom>'}
+    dom = vdom.mount_tag(root, tag, {'txt': 'hello world'})
     assert dom and dom.uuid # dom created
     assert vdom.get_dom(dom.uuid) # dom cached
     assert root.html() # mounted something
 
 def test_mount():
-    root = PyQuery('<body><div id="selector"><selected></body>')
-    vdom.define_tag('test', '<test>abc</test>', lambda *a, **kw: None)
+    root = PyQuery('<body><div id="selector"></div></body>')
+    vdom.define_tag('test', '<test><text>_</text></test>')
     doms = vdom.mount(root, '#selector', 'test')
     assert doms
-
-def test_walk():
-    root = PyQuery('<root><child><grandson></grandson></child></root>')
-    def fn(node):
-        node.addClass('walked')
-        return True
-    vdom.walk(root, fn)
-    assert str(root) == '<root class="walked"><child class="walked"><grandson class="walked"/></child></root>'
+    assert root('#selector').attr.__riot_tag__ == 'test'
