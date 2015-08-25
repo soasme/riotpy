fd72eefd1a43ba64f3571570ae46c2b281a1b55f
soasme <soasme@gmail.com>
2015-08-25 08:43:54 +0800

    Pass root to parse_node.

--- riot/expression.py
+++ riot/expression.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-
+import sys
 from .template import render_template
 from .utils import walk
+
+NODES = {}
 
 def parse_children(children, root, vnode):
     # walk(root, lambda node: parse_node_children(children, node, vnode))
@@ -17,28 +19,32 @@ def add_expression(expressions, dom, val, extra={}):
         expression.update(extra or {})
         expressions.append(expression)
 
-def parse_node(expressions, node):
+def parse_node(expressions, root, node):
     from .virtual_dom import is_tag_defined
     if node[0].tag == 'text':
-        add_expression(expressions, node, node.html())
+        add_expression(expressions, node, node.html(), dict(root=root))
         return False
     else:
         for attribute, val in node[0].attrib.items():
-            add_expression(expressions, node, val, dict(attr=attribute))
+            add_expression(expressions, node, val, dict(root=root, attr=attribute))
         return not is_tag_defined(node.attr.__riot_tag__)
 
 def parse_expressions(expressions, root):
-    walk(root.dom, lambda node: parse_node(expressions, node))
+    walk(root.dom, lambda node: parse_node(expressions, root, node))
 
 def update_expressions(expressions, node):
+    from .tags.text import parse_markup, META as TEXT_META
     for expression in expressions:
         dom = expression['dom']
         expr = expression['expr']
         attr = expression.get('attr')
+        root = expression.get('root')
         value = render_template(expr, node) or ''
+
         # parent = dom.parent()
         if expression.get('value') == value:
             continue
+
         expression['value'] = value
         if not attr:
             dom.html(value)
@@ -53,4 +59,4 @@ def update_expressions(expressions, node):
                 return ret
             setattr(node.ui, attr, new_callback)
             continue
-        getattr(node.ui.original_widget, 'set_%s' % attr)(value)
+        getattr(node.ui, 'set_%s' % attr)(value)
