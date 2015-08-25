ed9ff2bfc339be81f5aefefaf56727989a8606f8
soasme <soasme@gmail.com>
2015-08-25 17:20:09 +0800

    Jinja suites case nice.

--- riot/template.py
+++ riot/template.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import re
+from jinja2 import Environment
+from .observable import Observable
 
 def get_attribute(render_data, variable):
     levels = variable.split('.')
@@ -20,7 +22,14 @@ def is_function(text, node):
     fnname = render_template(text, node)
     return hasattr(fnname, node)
 
+env = Environment(variable_start_string='{', variable_end_string='}')
+
 def render_template(text, render_data):
+    if not (text.startswith('{') and text.endswith('}')):
+        return env.from_string(text).render(**vars(render_data))
+
+    expr = text[1:-1]
+    return env.compile_expression(expr)(**vars(render_data))
     variables = variable_re.findall(text)
     variables = {var[1:-1].strip(): var for var in variables}
     for variable in variables:
