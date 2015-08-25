ac119a42c495637b0a8697fa839033d8a47103e0
soasme <soasme@gmail.com>
2015-08-25 10:41:29 +0800

    Fix get_ui_by_path.

--- riot/utils.py
+++ riot/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import sys
 
 def walk(root, function, path=None):
     if not root:
@@ -12,14 +13,18 @@ def walk(root, function, path=None):
 def get_ui_by_path(root, path):
     import urwid
     ui = root
-    for level in path:
+    for level in path[1:]:
         if level == 0:
             if isinstance(ui, urwid.Filler):
                 ui = ui.body
             elif isinstance(ui, urwid.Pile):
-                continue
+                ui = ui.contents[0][0]
+            elif isinstance(ui, urwid.AttrMap):
+                ui = ui.base_widget
             else:
                 raise NotImplementedError(ui)
         else:
             ui = ui.contents[level][0]
+    if isinstance(ui, urwid.AttrMap):
+        return ui.base_widget
     return ui
