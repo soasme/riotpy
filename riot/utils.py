006068f6542db2ed89d07b99789d705162f949fc
soasme <soasme@gmail.com>
2015-08-25 09:07:52 +0800

    Walk and write path.

--- riot/utils.py
+++ riot/utils.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 
-def walk(root, function):
+def walk(root, function, path=None):
     if not root:
         return
-    if not function(root):
+    path = path or []
+    if not function(root, path):
         return
-    for child in root.children().items():
-        walk(child, function)
+    for index, child in enumerate(root.children().items()):
+        walk(child, function, path + [index])
+
+def get_ui_by_path(root, path):
+    import urwid
+    ui = root
+    for level in path:
+        if level == 0:
+            if isinstance(ui, urwid.Filler):
+                ui = ui.body
+            elif isinstance(ui, urwid.Pile):
+                continue
+            else:
+                raise NotImplementedError(ui)
+        else:
+            ui = ui.contents[level][0]
+    return ui
