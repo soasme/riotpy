7a7e3e034b52eb503ea89e4466ad5353991eebdc
soasme <soasme@gmail.com>
2015-08-26 16:02:56 +0800

    Detect if.

--- riot/tags/utils.py
+++ riot/tags/utils.py
@@ -4,6 +4,8 @@ from functools import wraps
 from urwid import AttrMap
 from pyquery import PyQuery
 
+from ..ui import IfWidget
+
 def convert_string_to_node(string):
     return PyQuery(string)
 
@@ -17,5 +19,17 @@ def detect_class(f):
         for class_name in class_names:
             if class_name:
                 node = AttrMap(node, class_name)
+        return node
+    return _detect_class
+
+def detect_if(f):
+    @wraps(f)
+    def _detect_class(*args, **kwargs):
+        pq = kwargs.get('node', args[0])
+        if_ = pq.attr['if'] or ''
+        state = if_ == 'True'
+        node = f(*args, **kwargs)
+        if if_:
+            node = IfWidget(node, state=state)
         return node
     return _detect_class
